ef787340adbf29ca86e2974ff7912be247175fab
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-04 15:49:58 +0100

    Add POST/ api endpoint
    In this commit, I added a POST api endpoint for creating a new accont record. This endpoint
    expects two fields (name and budget fields) to be inserted into the db.

--- Router/accountsRouter.js
+++ Router/accountsRouter.js
@@ -18,9 +18,23 @@ router.get('/', (req, res) => {
 router.get('/:id', async (req, res) => {
     try {
       const result = await db('accounts').where({ id: req.params.id });
-      res.json(result[0]);
+      res.status(200).json(result[0]);
     } catch (error) {
       res.status(500).json({ message: 'this went wrong: ' + error.message });
+    }
+  });
+
+  router.post('/', async (req, res) => {
+      const { name, budget } = req.body;
+    try {
+      const result = await db('accounts')
+        .insert({
+          name,
+          budget
+        })
+      res.status(201).json('New Account record got created with an id of ' + result[0]);
+    } catch (error) {
+      res.status(500).json({ message: 'this went wrong while trying to insert this account record ' + error.message });
     }
   });
 
