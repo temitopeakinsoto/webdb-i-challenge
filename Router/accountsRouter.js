f304d72849c24a0d66a85abf30f704769e88d978
Temitope Akinsoto <takinsoto@gmail.com>
2019-11-04 15:39:11 +0100

    Add GET /:id api endpoint
    In this commit, I added the GET /:id api endpoint for retrieve account details of a specified account id

--- Router/accountsRouter.js
+++ Router/accountsRouter.js
@@ -11,8 +11,17 @@ router.get('/', (req, res) => {
       res.json(result);
     })
     .catch(error => {
-      res.status(500).json({ message: 'this went wrong: ' + error.message });
+      res.status(500).json({ message: 'There was an error retrieving this data: ' + error.message });
     });
 });
+
+router.get('/:id', async (req, res) => {
+    try {
+      const result = await db('accounts').where({ id: req.params.id });
+      res.json(result[0]);
+    } catch (error) {
+      res.status(500).json({ message: 'this went wrong: ' + error.message });
+    }
+  });
 
 module.exports = router;
